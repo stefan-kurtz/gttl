088d66b6fdc4dee0e517da16b325ced2949252aa
Stefan Kurtz <kurtz@zbh.uni-hamburg.de>
2021-06-01 14:38:45 +0200

    Use two tasks lists instead of one

--- src/utilities/all_vs_all.hpp
+++ src/utilities/all_vs_all.hpp
@@ -6,14 +6,14 @@
 
 template<typename T,typename R,typename Data,
          size_t (*first_index)(size_t),
-         R (*process_pair)(T &,size_t,size_t),
+         R (*process_pair)(T &,size_t,T &,size_t),
          void (*process_result)(size_t,size_t,size_t,R,Data &)>
 static void gttl_one_vs_all(Data &data,size_t thread_id,
-                            size_t idx,T &tasks)
+                            T &tasks0,size_t idx,T &tasks1)
 {
-  for (size_t j = first_index(idx); j < tasks.size(); j++)
+  for (size_t j = first_index(idx); j < tasks1.size(); j++)
   {
-    process_result(thread_id,idx,j,process_pair(tasks,idx,j),data);
+    process_result(thread_id,idx,j,process_pair(tasks0,idx,tasks1,j),data);
   }
 }
 
@@ -21,17 +21,18 @@ template<typename T,typename Data>
 class GttlThreadData
 {
   public:
-    T &tasks;
+    T &tasks0, &tasks1;
     Data &data;
-  GttlThreadData(T &_tasks,Data &_d) :
-    tasks(_tasks),
+  GttlThreadData(T &_tasks0,T &_tasks1,Data &_d) :
+    tasks0(_tasks0),
+    tasks1(_tasks1),
     data(_d)
   {
   }
 };
 
 template<typename T,typename R,typename Data,size_t (*first_index)(size_t),
-         R (*process_pair)(T &,size_t,size_t),
+         R (*process_pair)(T &,size_t,T &,size_t),
          void (*process_result)(size_t,size_t,size_t,R,Data &)>
 static void gttl_thread_runner(size_t thread_id,size_t task_num,
                                void *v_thread_data)
@@ -39,27 +40,27 @@ static void gttl_thread_runner(size_t thread_id,size_t task_num,
   GttlThreadData<T,Data> *thread_data
     = static_cast<GttlThreadData<T,Data> *>(v_thread_data);
   gttl_one_vs_all<T,R,Data,first_index,process_pair,process_result>
-                      (thread_data->data,thread_id,task_num,
-                       thread_data->tasks);
+                      (thread_data->data,thread_id,
+                       thread_data->tasks0,task_num,thread_data->tasks1);
 }
 
 template<typename T,typename R,typename Data,
-         size_t (*first_index)(size_t),R (*process_pair)(T &,size_t,size_t),
+         size_t (*first_index)(size_t),R (*process_pair)(T &,size_t,T &,size_t),
          void (*process_result)(size_t,size_t,size_t,R,Data &)>
-void gttl_all_vs_all(T &tasks,Data &data,size_t num_threads=1)
+void gttl_all_vs_all(T &tasks0,T &tasks1,Data &data,size_t num_threads=1)
 {
   assert(num_threads >= 1);
   if (num_threads == 1)
   {
-    for (size_t idx = 0; idx < tasks.size(); idx++)
+    for (size_t idx = 0; idx < tasks0.size(); idx++)
     {
       gttl_one_vs_all<T,R,Data,first_index,process_pair,process_result>
-                        (data,0,idx,tasks);
+                        (data,0,tasks0,idx,tasks1);
     }
   } else
   {
-    GttlThreadData<T,Data> thread_data(tasks,data);
-    GttlThreadPool thread_pool(num_threads,tasks.size(),
+    GttlThreadData<T,Data> thread_data(tasks0,tasks1,data);
+    GttlThreadPool thread_pool(num_threads,tasks0.size(),
                                gttl_thread_runner<T,R,Data,first_index,
                                                   process_pair,
                                                   process_result>,
