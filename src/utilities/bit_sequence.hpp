4215b10a9cb95f3702f645f92c74d153dbf63ffb
Stefan Kurtz <stefan.kurtz@uni-hamburg.de>
2021-11-11 15:16:57 +0100

    Add functions for conversion from bitsequence to string

---
+++ src/utilities/bit_sequence.hpp
@@ -0,0 +1,36 @@
+#ifndef BITSEQUENCE_HPP
+#define BITSEQUENCE_HPP
+
+#include <string>
+#include <vector>
+#include <climits>
+
+template<typename Basetype>
+inline std::string bit_sequence2string(Basetype bs)
+{
+  const unsigned int bits = CHAR_BIT * sizeof(Basetype);
+  Basetype mask;
+  std::string buffer{};
+
+  buffer.reserve(bits);
+  for (mask = (static_cast<Basetype>(1) << (bits - 1)); mask > 0; mask >>= 1)
+  {
+    buffer += (bs & mask) ? '1' : '0';
+  }
+  return buffer;
+}
+
+template<typename Basetype>
+inline std::vector<std::string>
+   bit_sequence2string_vector(const Basetype *encoding,size_t num_bytes)
+{
+  std::vector<std::string> s_vec{};
+  s_vec.reserve(num_bytes);
+  for (size_t idx = 0; idx < num_bytes; idx++)
+  {
+    auto bs = bit_sequence2string<uint8_t>(encoding[idx]);
+    s_vec.push_back(bs);
+  }
+  return s_vec;
+}
+#endif
