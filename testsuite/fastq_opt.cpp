5506461cd21baec5e58364ca00c3a932d5d41c18
Stefan Kurtz <stefan.kurtz@uni-hamburg.de>
2022-04-14 14:58:12 +0200

    Split off option parser of fastq_mn and move to own module

---
+++ testsuite/fastq_opt.cpp
@@ -0,0 +1,117 @@
+/*
+  Copyright (c) 2021 Stefan Kurtz <kurtz@zbh.uni-hamburg.de>
+  Copyright (c) 2021 Center for Bioinformatics, University of Hamburg
+
+  Permission to use, copy, modify, and distribute this software for any
+  purpose with or without fee is hereby granted, provided that the above
+  copyright notice and this permission notice appear in all copies.
+
+  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+#include <cstddef>
+#include <iostream>
+#include <fstream>
+#include <cassert>
+#include <algorithm>
+#include "utilities/cxxopts.hpp"
+#include "fastq_opt.hpp"
+
+static void usage(const cxxopts::Options &options)
+{
+  std::cerr << options.help() << std::endl;
+}
+
+FastQReaderOptions::FastQReaderOptions(void) {};
+
+void FastQReaderOptions::parse(int argc, char **argv)
+{
+  cxxopts::Options options(argv[0],"process fastq files, optionally output "
+                                   "statistics, echo in the input or show "
+                                   "sequences in fasta file");
+  options.set_width(80);
+  options.custom_help(std::string("[options] filename0 [filename1]"));
+  options.set_tab_expansion();
+  options.add_options()
+     ("s,statistics", "output statistics",
+      cxxopts::value<bool>(statistics_option)->default_value("false"))
+     ("e,echo", "available only for single file",
+      cxxopts::value<bool>(echo_option)->default_value("false"))
+     ("split_size", "specify number of sequences for each split",
+      cxxopts::value<size_t>(split_size)->default_value("0"))
+     ("f,fasta_output", "output sequences in fasta format; when two files "
+                        "are specified, the sequences are zipped, i.e. "
+                        "sequences at even indexes (when counting from 0) "
+                        "are from the first file and sequences at odd "
+                        "indexes are from the second file",
+      cxxopts::value<bool>(fasta_output_option)->default_value("false"))
+     ("h,help", "print usage");
+  try
+  {
+    auto result = options.parse(argc, argv);
+    if (result.count("help") > 0)
+    {
+      help_option = true;
+      usage(options);
+    }
+    const std::vector<std::string>& unmatched_args = result.unmatched();
+    for (size_t idx = 0; idx < unmatched_args.size(); idx++)
+    {
+      inputfiles.push_back(unmatched_args[idx]);
+    }
+    if (inputfiles.size() < 1)
+    {
+      throw cxxopts::OptionException("not enough input files");
+    }
+    if (inputfiles.size() > 2)
+    {
+      throw cxxopts::OptionException("superfluous input files");
+    }
+    if (split_size != 0 && inputfiles.size() != 1)
+    {
+      throw cxxopts::OptionException("option --splitsize_t is only available "
+                                     "for a single file");
+    }
+  }
+  catch (const cxxopts::OptionException &e)
+  {
+    usage(options);
+    throw std::invalid_argument(e.what());
+  }
+}
+
+bool FastQReaderOptions::help_option_is_set(void) const noexcept
+{
+  return help_option;
+}
+
+bool FastQReaderOptions::statistics_option_is_set(void) const noexcept
+{
+  return statistics_option;
+}
+
+bool FastQReaderOptions::echo_option_is_set(void) const noexcept
+{
+  return echo_option;
+}
+
+bool FastQReaderOptions::fasta_output_option_is_set(void) const noexcept
+{
+  return fasta_output_option;
+}
+
+size_t FastQReaderOptions::split_size_get(void) const noexcept
+{
+  return split_size;
+}
+
+const std::vector<std::string> &FastQReaderOptions::inputfiles_get(void)
+      const noexcept
+{
+  return inputfiles;
+}
---
+++ testsuite/fastq_opt.hpp
@@ -0,0 +1,27 @@
+#ifndef FASTQ_OPT_HPP
+#define FASTQ_OPT_HPP
+#include <cstddef>
+#include <cstdbool>
+#include <string>
+#include <vector>
+
+class FastQReaderOptions
+{
+ private:
+  std::vector<std::string> inputfiles{};
+  bool help_option = false, statistics_option = false,
+       echo_option = false, fasta_output_option = false;
+  size_t split_size = 0;
+
+ public:
+  FastQReaderOptions(void);
+
+  void parse(int argc, char **argv);
+  bool help_option_is_set(void) const noexcept;
+  bool statistics_option_is_set(void) const noexcept;
+  bool echo_option_is_set(void) const noexcept;
+  bool fasta_output_option_is_set(void) const noexcept;
+  size_t split_size_get(void) const noexcept;
+  const std::vector<std::string> &inputfiles_get(void) const noexcept;
+};
+#endif
