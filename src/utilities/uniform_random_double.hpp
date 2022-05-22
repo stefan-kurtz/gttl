f28ca1bb7eb657db22354043812ac1ae2d1afef7
Stefan Kurtz <stefan.kurtz@uni-hamburg.de>
2022-05-22 15:01:27 +0200

    Allow optional own seed for random number generator

--- src/utilities/uniform_random_double.hpp
+++ src/utilities/uniform_random_double.hpp
@@ -5,27 +5,29 @@
 
 class UniformRandomDouble
 {
-  std::random_device _rd{};
-  std::mt19937 _gen{_rd()};
-  std::uniform_real_distribution<double> _dist;
+  std::random_device seed{};
+  std::mt19937 generator;
+  std::uniform_real_distribution<double> distribution;
 
   void set(double low, double high)
   {
     std::uniform_real_distribution<double>::param_type param(low, high);
-    _dist.param(param);
+    distribution.param(param);
   }
   public:
-  UniformRandomDouble(void)
+  UniformRandomDouble(unsigned int own_seed = 0)
+    : generator(own_seed == 0 ? seed() : own_seed)
   {
     set(DBL_MIN, DBL_MAX);
   }
-  UniformRandomDouble(double low, double high)
+  UniformRandomDouble(double low, double high, unsigned int own_seed = 0)
+    : generator(own_seed == 0 ? seed() : own_seed)
   {
     set(low, high);
   }
   double get(void)
   {
-    return _dist(_gen);
+    return distribution(generator);
   }
 };
 #endif
