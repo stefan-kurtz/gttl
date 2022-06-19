454a6e8fd59087561117cf65292744c53fcc60be
Stefan Kurtz <stefan.kurtz@uni-hamburg.de>
2022-06-19 16:57:47 +0200

    make sort_key_value_pairs.cpp more reusable

--- testsuite/sort_key_value_pairs.cpp
+++ testsuite/sort_key_value_pairs.cpp
@@ -14,48 +14,99 @@
 
 class KeyValuePair
 {
-  union
-  {
-    double d;
-    unsigned char ds[sizeof(double)];
-  } key;
+  double key;
   size_t value;
   public:
+  KeyValuePair(void)
+    : key(0)
+    , value(0)
+  {}
   KeyValuePair(double _key,size_t _value)
+    : key(_key)
+    , value(_value)
+  {}
+  double key_get(void) const noexcept
   {
-    this->key.d = _key;
-    this->value = _value;
+    return key;
   }
-  double double_get(void) const noexcept
+  std::string to_string(void) const noexcept
   {
-    return key.d;
-  }
-  void show(void) const noexcept
-  {
+    std::string s{};
+    const unsigned char *ds = reinterpret_cast<const unsigned char *>(&key);
     for (size_t idx = 0; idx < sizeof(double); idx++)
     {
-      printf("%03d",static_cast<int>(key.ds[idx]));
+      char sbuf[3+1];
+      sprintf(sbuf,"%03d",static_cast<int>(ds[idx]));
+      s += std::string(sbuf);
       if (idx < sizeof(double) - 1)
       {
-        printf(" ");
+        s += " ";
       }
     }
+    return s;
   }
   bool operator < (const KeyValuePair& other) const noexcept
   {
-    return key.d < other.key.d;
+    return key < other.key;
   }
 };
 
-void show_key_values_pairs(const std::vector<KeyValuePair> &key_value_pairs)
+template<class T>
+static void sort_values(const char *progname,bool use_radix_sort,
+                        size_t number_of_values,
+                        int num_sort_bits)
 {
-  size_t idx = 0;
-  for (auto &&kv : key_value_pairs)
+  std::vector<T> values{};
+  values.reserve(number_of_values);
+  UniformRandomDouble urd_gen(0,DBL_MAX);
+  RunTimeClass rt_random_number_generation{};
+  for (size_t idx = 0; idx < number_of_values; idx++)
   {
-    printf("%03lu\t",idx);
-    kv.show();
-    printf("\n");
-    idx++;
+    double r = urd_gen.get();
+    values.push_back(T(r,idx));
+  }
+  rt_random_number_generation.show("random number generation");
+  std::cout << "# size of array (MB):\t"
+            << (mega_bytes(values.size() * sizeof(T)))
+            << std::endl;
+  RunTimeClass rt_sorting{};
+  if (use_radix_sort)
+  {
+    static constexpr const int sizeof_unit
+      = static_cast<int>(sizeof(T));
+    const bool reversed_byte_order = is_big_endian() ? false : true;
+    ska_large_lsb_small_radix_sort(sizeof_unit,
+                                   num_sort_bits,
+                                   reinterpret_cast<uint8_t *>(values.data()),
+                                   values.size(),
+                                   reversed_byte_order);
+    StrFormat msg("sort %lu values with ska_large_lsb_small_radix_sort",
+                  values.size());
+    rt_sorting.show(msg.str());
+  } else
+  {
+    std::sort(values.begin(),values.end());
+    StrFormat msg("sort %lu values with std::sort",values.size());
+    rt_sorting.show(msg.str());
+  }
+#ifdef SHOW_VALUES
+  for (auto v : values)
+  {
+    std::cout << v.to_string() << std::endl;
+  }
+#endif
+  T previous;
+  bool previous_defined = false;
+  for (auto v : values)
+  {
+    if (previous_defined && v < previous)
+    {
+      std::cerr << progname << ": previous=" << previous.to_string() << " > "
+                << v.to_string() << "=next" << std::endl;
+      exit(EXIT_FAILURE);
+    }
+    previous = v;
+    previous_defined = true;
   }
 }
 
@@ -74,57 +125,11 @@ int main(int argc, char *argv[])
     return EXIT_FAILURE;
   }
   const bool use_radix_sort = strcmp(argv[1],"rad") == 0 ? true : false;
-  size_t number_of_key_value_pairs = static_cast<size_t>(readlong);
+  const size_t number_of_values = static_cast<size_t>(readlong);
   static_assert(sizeof(KeyValuePair) == 2 * sizeof(void *));
-  std::vector<KeyValuePair> key_value_pairs{};
-  key_value_pairs.reserve(number_of_key_value_pairs);
-  UniformRandomDouble urd_gen(0,DBL_MAX);
-  RunTimeClass rt_random_number_generation{};
-  for (size_t idx = 0; idx < number_of_key_value_pairs; idx++)
-  {
-    double r = urd_gen.get();
-    key_value_pairs.push_back(KeyValuePair(r,idx));
-  }
-  rt_random_number_generation.show("random number generation");
-  std::cout << "# size of array with key value pairs (MB):\t"
-            << (mega_bytes(key_value_pairs.size() * sizeof(KeyValuePair)))
-            << std::endl;
-  RunTimeClass rt_sorting{};
-  if (use_radix_sort)
-  {
-    static constexpr const int sizeof_unit
-      = static_cast<int>(sizeof(KeyValuePair));
-    static constexpr const int num_sort_bits
-      = static_cast<int>(CHAR_BIT * sizeof(double));
-    const bool reversed_byte_order = is_big_endian() ? false : true;
-    ska_large_lsb_small_radix_sort(sizeof_unit,
-                                   num_sort_bits,
-                                   reinterpret_cast<uint8_t *>
-                                     (key_value_pairs.data()),
-                                   key_value_pairs.size(),
-                                   reversed_byte_order);
-    StrFormat msg("sort %lu values with ska_large_lsb_small_radix_sort",
-                  key_value_pairs.size());
-    rt_sorting.show(msg.str());
-  } else
-  {
-    std::sort(key_value_pairs.begin(),key_value_pairs.end());
-    StrFormat msg("sort %lu values with std::sort",key_value_pairs.size());
-    rt_sorting.show(msg.str());
-  }
-#ifdef SHOW_VALUES
-  show_key_values_pairs(key_value_pairs);
-#endif
-  double previous = DBL_MIN;
-  for (auto &&kv : key_value_pairs)
-  {
-    if (previous > kv.double_get())
-    {
-      std::cerr << argv[0] << ": previous=" << previous << " > "
-                << kv.double_get() << "=next" << std::endl;
-      return EXIT_FAILURE;
-    }
-    previous = kv.double_get();
-  }
+  static constexpr const int num_sort_bits
+    = static_cast<int>(CHAR_BIT * sizeof(double));
+  sort_values<KeyValuePair>(argv[0],use_radix_sort,number_of_values,
+                            num_sort_bits);
   return EXIT_SUCCESS;
 }
