a321f396a09b011c1334f791cd6124741ab03b51
Stefan Kurtz <stefan.kurtz@uni-hamburg.de>
2022-11-17 01:02:00 +0100

    Add optional skip option.

--- src/utilities/split_string.hpp
+++ src/utilities/split_string.hpp
@@ -4,8 +4,10 @@
 #include <cstdbool>
 
 template<typename T,T convert(size_t,const std::string &)>
-static std::vector<T> split_string(const std::string &str, char sep)
+static std::vector<T> split_string(const std::string &str,char sep,
+                                   int skip = 1)
 {
+  assert(skip >= 1);
   auto previous = str.begin();
   std::vector<T> result{};
   while (true)
@@ -19,7 +21,7 @@ static std::vector<T> split_string(const std::string &str, char sep)
     }
     std::string this_string = std::string(previous,next);
     result.push_back(convert(result.size(),this_string));
-    previous = next + 1;
+    previous = next + skip;
   }
   return result;
 }
