bb2cd058702bb62754ccc4148e4bcb602811ee86
Stefan Kurtz <stefan.kurtz@uni-hamburg.de>
2023-04-24 23:53:45 +0200

    Added sorted_match.hpp

---
+++ src/sequences/stored_match.hpp
@@ -0,0 +1,121 @@
+#ifndef STORED_MATCH_HPP
+#define STORED_MATCH_HPP
+#include <string>
+#include <cstdint>
+#include <cassert>
+
+/* This class represents coordinates of matches for the same segment,
+   i.e. for the same pair of reference and query sequence. So the
+   ID of the sequences need not be stored. The coordinates of the match
+   on the primary sequence, given by a start position and a length
+   is used to resolve ties when comparing matches for which the
+   weights are equal. The weight is a numerical value used to
+   order stored matches. The order may be ascending, i.e. the larger the
+   weight, the better. This order is used when the template variable
+   argest_weight_best is true. The order may be descending, i.e. the
+   smaller the better. This order is used when the template variable
+   argest_weight_best is false.
+ */
+
+class GttlStoredMatch
+{
+  private:
+  static constexpr const size_t max_value = UINT32_MAX;
+  const uint32_t primary_startpos,
+                 primary_len,
+                 secondary_startpos,
+                 secondary_len,
+                 distance;
+  const float weight;
+
+  double weight_get(void) const noexcept
+  {
+    return weight;
+  }
+
+  public:
+  GttlStoredMatch(size_t _primary_startpos, size_t _primary_len,
+                  size_t _secondary_startpos, size_t _secondary_len,
+                  size_t _distance,
+                  double _weight)
+    : primary_startpos(_primary_startpos)
+    , primary_len(_primary_len)
+    , secondary_startpos(_secondary_startpos)
+    , secondary_len(_secondary_len)
+    , distance(_distance)
+    , weight(_weight)
+  {
+    assert(_primary_startpos <= max_value &&
+           _primary_len <= max_value &&
+           _secondary_startpos <= max_value &&
+           _secondary_len <= max_value &&
+           _distance <= max_value);
+  }
+  uint32_t primary_startpos_get(void) const noexcept
+  {
+    return primary_startpos;
+  }
+  uint32_t primary_len_get(void) const noexcept
+  {
+    return primary_len;
+  }
+  uint32_t secondary_startpos_get(void) const noexcept
+  {
+    return secondary_startpos;
+  }
+  uint32_t secondary_len_get(void) const noexcept
+  {
+    return secondary_len;
+  }
+  uint32_t distance_get(void) const noexcept
+  {
+    return distance;
+  }
+  /* the == and != operators are needed to check the consistency of the filter
+     results in verify_filer */
+  bool operator==(const GttlStoredMatch& other) const noexcept
+  {
+    return primary_startpos == other.primary_startpos and
+           primary_len == other.primary_len and
+           secondary_startpos == other.secondary_startpos and
+           secondary_len == other.secondary_len and
+           distance == other.distance;
+  }
+  bool operator != (const GttlStoredMatch& other) const noexcept
+  {
+    return not (*this == other);
+  }
+
+  /* compare by weight only */
+  template<bool largest_weight_best>
+  bool superior_weight (const GttlStoredMatch& other) const noexcept
+  {
+    if constexpr (largest_weight_best)
+    {
+      return this->weight_get() > other.weight_get();
+    } else
+    {
+      return this->weight_get() < other.weight_get();
+    }
+  }
+
+  /* compare weights and if a tie, compare by primary startpos */
+  template<bool largest_weight_best>
+  bool superior_weight_tie_primary_startpos(const GttlStoredMatch& other)
+    const noexcept
+  {
+      return this->superior_weight<largest_weight_best>(other) or
+             (this->weight_get() == other.weight_get() and
+              this->primary_startpos_get() > other.primary_startpos_get());
+  }
+
+  std::string to_string(void) const noexcept
+  {
+    return std::to_string(primary_startpos) + "," +
+           std::to_string(primary_len) + "," +
+           std::to_string(secondary_startpos) + "," +
+           std::to_string(secondary_len) + "," +
+           std::to_string(weight);
+  }
+};
+#endif
