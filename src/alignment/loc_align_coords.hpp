95b19efb7e7692be0cad803f5cc9aaac8daa33c9
Stefan Kurtz <stefan.kurtz@uni-hamburg.de>
2023-08-13 22:35:14 +0200

    Optional output of strand information.

--- src/alignment/loc_align_coords.hpp
+++ src/alignment/loc_align_coords.hpp
@@ -13,13 +13,36 @@ struct LocalAlignmentCoordinates
          vsubstringlength; /* length of substring of local opt. align.
                               in <vseq> */
   uint32_t raw_score; /* score of local alignment */
-  void show(FILE *fpout) const noexcept
+  bool forward_strand;
+  void show(FILE *fpout,bool dna_alphabet,size_t vlen) const noexcept
   {
+    size_t vstart_out;
+    if (forward_strand)
+    {
+      vstart_out = vstart;
+    } else
+    {
+      assert(vlen >= vstart + vsubstringlength);
+      vstart_out = vlen - vstart - vsubstringlength;
+    }
     fprintf(fpout,"%lu\t%lu\t%lu\t%lu\t%u",
             ustart,
             usubstringlength,
-            vstart,
+            vstart_out,
             vsubstringlength,raw_score);
+    if (dna_alphabet)
+    {
+      fprintf(fpout,"\t%c",forward_strand ? '+' : '-');
+    }
+  }
+  bool better(const LocalAlignmentCoordinates &other,
+              bool compute_only_end) const noexcept
+  {
+    return raw_score > other.raw_score ||
+           (not compute_only_end &&
+            raw_score == other.raw_score &&
+            usubstringlength + vsubstringlength >
+            other.usubstringlength + other.vsubstringlength);
   }
 };
 #endif
