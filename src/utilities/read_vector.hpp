bc8b9def12a55b3424420bdefbe72d61201fe965
Stefan Kurtz <stefan.kurtz@uni-hamburg.de>
2024-07-10 14:06:17 +0200

    Reordered allocation of vector

--- src/utilities/read_vector.hpp
+++ src/utilities/read_vector.hpp
@@ -19,8 +19,6 @@ std::vector<T> gttl_read_vector(const char *filename)
                   filename,size_of_file,sizeof(T));
     throw msg.str();
   }
-  const size_t num_values = size_of_file/sizeof(T);
-  std::vector<T> vec(num_values);
   // Open the stream to 'lock' the file.
   std::ifstream instream(filename, std::ios::in | std::ios::binary);
   if (instream.fail())
@@ -28,6 +26,8 @@ std::vector<T> gttl_read_vector(const char *filename)
     StrFormat msg("cannot open file %s",filename);
     throw msg.str();
   }
+  const size_t num_values = size_of_file/sizeof(T);
+  std::vector<T> vec(num_values);
   if (!instream.read(reinterpret_cast<char*>(vec.data()), size_of_file))
   {
     StrFormat msg("cannot only read %lu bytes from file %s",
