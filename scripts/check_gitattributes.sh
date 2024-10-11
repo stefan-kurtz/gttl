6d98acea2058d3d08764b9bce1590ca8d5610ea2
Stevan Miladinovic <bbb7752@studium.uni-hamburg.de>
2024-10-11 17:36:32 +0200

    Follow PORTABILITY guidelines in check_gitattributes.sh

--- scripts/check_gitattributes.sh
+++ scripts/check_gitattributes.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# Checks that all files in the current git-repository are mentioned in
-# .gitattributes by checking whether their `text` attribute was set or unset
+# Checks that all files in the current git-repository are matched by a pattern in
+# .gitattributes. 
+# This is done by checking whether their `text` attribute was
+# explicitly set or unset. 
 # Exits with return-code 1 if any checked-in files are not missing, 0 otherwise.
 # This automatically ignores all files in `.gitignore`.
 # Used as part of `make code_check`.
 
 FILES_WITHOUT_ATTR=$(git ls-files | xargs git check-attr text |\
-  egrep -v "text: set|text: unset")
+  grep -v -E "text: set|text: unset")
 
 if [ -n "${FILES_WITHOUT_ATTR}" ]; then
   >&2 echo "The following Files were found in the repository, but are not mentioned in .gitattributes:"
