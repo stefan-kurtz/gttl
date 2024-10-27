9c7d140b23a6fe8f03508e0486dda078cd3917ec
Stevan Miladinovic <bbb7752@studium.uni-hamburg.de>
2024-10-27 22:30:07 +0100

    Add testsuite functionality for splitting into given number of sequences

--- testsuite/split_files_mn.cpp
+++ testsuite/split_files_mn.cpp
@@ -21,10 +21,13 @@ int main(int argc, char *argv[])
                         cxxopts::value<std::string>())(
       "h,help", "Print usage information")(
       "n,num_parts",
-      "Number of parts to split the file into (mutually exclusive with -l)",
+      "Number of parts to split the file into (mutually exclusive with -l, -s)",
       cxxopts::value<size_t>())(
       "l,len_parts",
-      "Length of parts to split the file into (mutually exclusive with -n)",
+      "Length of parts to split the file into (mutually exclusive with -n, -s)",
+      cxxopts::value<size_t>())(
+      "s,num_sequences",
+      "Number of sequences per file (mutually exclusive with -l, -n)",
       cxxopts::value<size_t>())(
       "o,output",
       "The output filenames (numbering and file-extensions will be added)",
@@ -49,22 +52,31 @@ int main(int argc, char *argv[])
     exit(EXIT_FAILURE);
   }
 
-  if (result.count("num_parts") + result.count("len_parts") != 1)
+  if (result.count("num_parts") + result.count("len_parts") +
+          result.count("num_sequences") !=
+      1)
   {
     std::cerr << argv[0] << ": Exactly one of -l and -n must be specified"
               << std::endl;
     exit(EXIT_FAILURE);
   }
 
-  size_t num_parts, len_parts;
+  size_t num_parts, len_parts, num_sequences;
   if (result.count("num_parts"))
   {
     num_parts = result["num_parts"].as<size_t>();
     len_parts = 0;
-  } else
+    num_sequences = 0;
+  } else if (result.count("len_parts"))
   {
     num_parts = 0;
     len_parts = result["len_parts"].as<size_t>();
+    num_sequences = 0;
+  } else
+  {
+    num_parts = 0;
+    len_parts = 0;
+    num_sequences = result["num_sequences"].as<size_t>();
   }
 
   std::string ifilename = result["file"].as<std::string>();
@@ -98,9 +110,18 @@ int main(int argc, char *argv[])
                            result["compression_level"].as<size_t>());
       return EXIT_SUCCESS;
     }
-    split_into_parts_length(fasta_it, output_basename, len_parts,
-                            result["compresssion_level"].as<size_t>());
-    return EXIT_SUCCESS;
+    if (len_parts != 0)
+    {
+      split_into_parts_length(fasta_it, output_basename, len_parts,
+                              result["compresssion_level"].as<size_t>());
+      return EXIT_SUCCESS;
+    }
+    if (num_sequences != 0)
+    {
+      split_into_num_sequences(fasta_it, output_basename, num_sequences,
+                               result["compression_level"].as<size_t>());
+      return EXIT_SUCCESS;
+    }
   } else if (gttl_has_suffix(ifilename, ".fastq") ||
              gttl_has_suffix(ifilename, ".fastq.gz"))
   {
@@ -112,9 +133,18 @@ int main(int argc, char *argv[])
                            result["compression_level"].as<size_t>());
       return EXIT_SUCCESS;
     }
-    split_into_parts_length(fastq_it, output_basename, len_parts,
-                            result["compression_level"].as<size_t>());
-    return EXIT_SUCCESS;
+    if (len_parts != 0)
+    {
+      split_into_parts_length(fastq_it, output_basename, len_parts,
+                              result["compression_level"].as<size_t>());
+      return EXIT_SUCCESS;
+    }
+    if (num_sequences != 0)
+    {
+      split_into_num_sequences(fastq_it, output_basename, num_sequences,
+                               result["compression_level"].as<size_t>());
+      return EXIT_SUCCESS;
+    }
   }
 
   std::cerr << argv[0] << ": file " << ifilename
