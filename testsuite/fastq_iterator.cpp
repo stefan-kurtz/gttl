a6e96e7bf19596a56c24679182d08c757850710d
Stevan Miladinovic <stevan.miladinovic@uni-hamburg.de>
2024-12-23 05:37:21 +0100

    Remove non-existing include

--- testsuite/fastq_iterator.cpp
+++ testsuite/fastq_iterator.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include "sequences/gttl_fastq_iterator.hpp"
 #include "utilities/gttl_line_iterator.hpp"
-#include "iterator_compare.hpp"
+
+#define FILENAME "../testdata/SRR19536726_1_1000.fastq.gz"
 
 int main()
 {
   size_t pseudo_hash = 0;
-  for(unsigned long long i = 0; i < ITERATIONS; i++)
-  {
     constexpr const size_t buf = (1 << 14);
     GttlLineIterator<buf> line_it(FILENAME);
     GttlFastQIterator<GttlLineIterator<buf>> fastq_it(line_it);
@@ -18,6 +17,5 @@ int main()
       pseudo_hash += std::ranges::count(q.sequence_get(), 'a');
       pseudo_hash += std::ranges::count(q.quality_get(), 'F');
     }
-  }
   std::cout << pseudo_hash << std::endl;
 }
