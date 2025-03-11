68d6baded9f2b9b6bf789930403914139b096b99
Stevan Miladinovic <stevan.miladinovic@uni-hamburg.de>
2025-03-11 06:24:34 +0100

    Use RunTimeClass in iterator_compare

--- testsuite/iterator_compare.cpp
+++ testsuite/iterator_compare.cpp
@@ -1,6 +1,5 @@
 #include <algorithm>
 #include <cassert>
-#include <chrono>
 #include <cstring>
 #include <iostream>
 #include "sequences/gttl_fasta_generator.hpp"
@@ -9,13 +8,10 @@
 #include "sequences/gttl_seq_iterator.hpp"
 #include "utilities/cxxopts.hpp"
 #include "utilities/gttl_line_iterator.hpp"
+#include "utilities/runtime_class.hpp"
 
-//TODO: Use utilities/RunTimeClass
 //TODO: valgrind
 //TODO: Test with very-large-lines
-
-using hrc = std::chrono::high_resolution_clock;
-using sec = std::chrono::seconds;
 
 inline size_t count_occ(const char* string, char symbol)
 {
@@ -69,6 +65,7 @@ int main(int argc, char *argv[])
 
   const char* file = result["file"].as<std::string>().c_str();
 
+  RunTimeClass runtime;
 
 
   if(result["fastq"].as<bool>())
@@ -76,68 +73,57 @@ int main(int argc, char *argv[])
     GttlLineIterator<buf_size> line_it(file);
     GttlFastQIterator<GttlLineIterator<buf_size>> fq_it(line_it);
 
-    auto start_it = hrc::now();
+    runtime.reset();
     for(auto& entry : fq_it)
     {
       pseudo_hash_it += count_occ(entry.header_get().data(), '@');
       pseudo_hash_it += count_occ(entry.sequence_get().data(), 'a');
       pseudo_hash_it += count_occ(entry.quality_get().data(), 'F');
     }
-    auto end_it = hrc::now();
-    auto duration_it = std::chrono::duration_cast<sec>(end_it - start_it);
-    std::cout << "Iterator method ran in:\t\t" <<
-        duration_it.count() << " seconds.\n";
+    runtime.show("Iterator method");
 
     GttlFastQGenerator<buf_size> fq_gen(file);
 
-    auto start_gen = hrc::now();
+    runtime.reset();
     for(auto entry : fq_gen)
     {
       pseudo_hash_gen += count_occ(entry->header, '@');
       pseudo_hash_gen += count_occ(entry->sequence, 'a');
       pseudo_hash_gen += count_occ(entry->quality, 'F');
     }
-    auto end_gen = hrc::now();
-    auto duration_gen = std::chrono::duration_cast<sec>(end_gen-start_gen);
+    runtime.show("Generator method");
 
+    std::cout << pseudo_hash_it << "\t" << pseudo_hash_gen << "\n";
     // If the hashes are not identical, a mistake exists in
     // at least one implementation.
     // Performance comparinsons make no sense when this is the case.
     assert(pseudo_hash_it == pseudo_hash_gen);
-    std::cout << "Generator method ran in:\t" <<
-        duration_gen.count() << " seconds.\n";
   }
   else if(result["fasta"].as<bool>())
   {
     GttlSeqIterator<buf_size> fa_it(file);
 
-    auto start_it = hrc::now();
+    runtime.reset();
     for(auto &entry : fa_it)
     {
       pseudo_hash_it += count_occ(entry.header_get().data(), '>');
       pseudo_hash_it += count_occ(entry.sequence_get().data(), 'a');
       pseudo_hash_it += count_occ(entry.sequence_get().data(), 'A');
     }
-    auto end_it = hrc::now();
-    auto duration_it = std::chrono::duration_cast<sec>(end_it - start_it);
-    std::cout << "Iterator method ran in:\t\t" <<
-      duration_it.count() << " seconds.\n";
+    runtime.show("Iterator method");
 
     GttlFastAGenerator<buf_size> fa_gen(file);
 
-    auto start_gen = hrc::now();
+    runtime.reset();
     for(auto entry : fa_gen)
     {
       pseudo_hash_gen += count_occ(entry->header, '>');
       pseudo_hash_gen += count_occ(entry->sequence, 'a');
       pseudo_hash_gen += count_occ(entry->sequence, 'A');
     }
-    auto end_gen = hrc::now();
-    auto duration_gen = std::chrono::duration_cast<sec>(end_gen - start_gen);
+    runtime.show("Generator method");
 
-    assert(pseudo_hash_it == pseudo_hash_gen);
     std::cout << pseudo_hash_it << "\t" << pseudo_hash_gen << "\n";
-    std::cout << "Generator method ran in:\t" <<
-      duration_gen.count() << " seconds.\n";
+    assert(pseudo_hash_it == pseudo_hash_gen);
   }
 }
