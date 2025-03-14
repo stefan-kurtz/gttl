e572c40ad90242826d3d7d233fd8062af4ca6af0
Stevan Miladinovic <stevan.miladinovic@uni-hamburg.de>
2025-03-14 16:27:30 +0100

    Add tools/chaining

---
+++ tools/chaining/chaining.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+#include "utilities/constexpr_for.hpp"
+#include "utilities/gttl_line_iterator.hpp"
+#include "utilities/split_string.hpp"
+// #include "sequences/stored_match.hpp"
+#include "stored_match.hpp"
+#include "chaining.hpp"
+#include "chaining_opt.hpp"
+
+int main(int argc, char *argv[])
+{
+  ChainingOptions options;
+  try
+  {
+    options.parse(argc, argv);
+  }
+  catch (const std::invalid_argument &e)
+  {
+    std::cerr << argv[0] << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+  if (options.help_option_is_set())
+  {
+    return EXIT_SUCCESS;
+  }
+
+  constexpr const int buf_size = 1 << 14;
+  std::string buffer{};
+  const std::string inputfile = options.inputfile_get();
+  const bool local_option = options.local_option_is_set();
+  const bool silent_option = options.silent_option_is_set();
+  std::vector<GttlStoredMatch> matches{};
+  try
+  {
+    GttlLineIterator<buf_size> gttl_li(inputfile.c_str());
+    while (gttl_li.next(&buffer))
+    {
+      std::vector<std::string> vec = gttl_split_string(buffer, ' ');
+      if (vec.size() != 5)
+      {
+        throw std::string(": line has ") + std::to_string(vec.size()) +
+              std::string(" columns, but 5 are expected");
+      }
+      matches.emplace_back(std::stoul(vec[0]),
+                           std::stoul(vec[1])-std::stoul(vec[0])+1,
+                           std::stoul(vec[2]),
+                           std::stoul(vec[3])-std::stoul(vec[2])+1,
+                           0,
+                           std::stoul(vec[4]));
+      buffer.clear();
+    }
+  }
+  catch (std::string &msg)
+  {
+    std::cerr << argv[0] << ": " << msg << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  constexpr_for<0,1+1,1>([&](auto compile_time_local_option)
+  {
+    if (compile_time_local_option == local_option)
+    {
+      Chain<GttlStoredMatch, compile_time_local_option> chaining(matches);
+      std::cout << "# chain: length " << chaining.size()
+                << " score " << chaining.score() << std::endl;
+      if (not silent_option)
+      {
+        for (auto idx : chaining)
+        {
+          std::cout << idx << " " << matches[idx].to_string() << std::endl;
+        }
+      }
+    }
+  });
+  return EXIT_SUCCESS;
+}
