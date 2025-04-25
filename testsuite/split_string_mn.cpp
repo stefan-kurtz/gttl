09dacc26034532f4511a3086564d9b1ef28a2249
Stefan Kurtz <stefan.kurtz@uni-hamburg.de>
2025-04-25 13:20:06 +0200

    Replaced GttlLineIterator by GttlLineGenerator

--- testsuite/split_string_mn.cpp
+++ testsuite/split_string_mn.cpp
@@ -1,38 +1,36 @@
 #include <cstddef>
 #include <iostream>
 #include <string>
+#include "utilities/str_format.hpp"
 #include "utilities/split_string.hpp"
 #include "utilities/concatenate_strings.hpp"
-#include "utilities/gttl_line_iterator.hpp"
+#include "utilities/gttl_line_generator.hpp"
 
-static std::pair<size_t,size_t> process_inputfile(const char *inputfile,
+static std::pair<size_t,size_t> test_split_string(const char *inputfile,
                                                   char sep)
 {
   constexpr const int buf_size = 1 << 14;
-  GttlLineIterator<buf_size> gttl_li(inputfile);
-  std::string buffer{};
+  GttlLineGenerator<buf_size> gttl_li(inputfile);
   size_t line_count = 0, column_count = 0;
-  while (gttl_li.next(&buffer))
+  for (auto &buffer : gttl_li)
   {
     if (buffer.size() > 0 and buffer[0] != '#')
     {
       std::vector<std::string> vec = gttl_split_string(buffer, sep);
       column_count += vec.size();
-      std::string sep_string{sep};
+      const std::string sep_string{sep};
       std::string line_from_vec = gttl_concatenate_strings(vec.begin(),
                                                            vec.end(),
                                                            sep_string);
-      line_from_vec += '\n';
       if (buffer != line_from_vec)
       {
-        StrFormat msg("'%s' != '%s'",buffer.c_str(),line_from_vec.c_str());
+        StrFormat msg(": '%s' != '%s'",buffer.c_str(),line_from_vec.c_str());
         throw msg.str();
       }
     }
-    buffer.clear();
     line_count++;
   }
-  return {line_count,column_count};
+  return std::make_pair(line_count,column_count);
 }
 
 int main(int argc,char *argv[])
@@ -51,7 +49,7 @@ int main(int argc,char *argv[])
   try
   {
     size_t line_count, column_count;
-    std::tie(line_count,column_count) = process_inputfile(inputfile,argv[1][0]);
+    std::tie(line_count,column_count) = test_split_string(inputfile,argv[1][0]);
     std::cout << "processed " << column_count << " columns in " << line_count
               << " lines" << std::endl;
   }
