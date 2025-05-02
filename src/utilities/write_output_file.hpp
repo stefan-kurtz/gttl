b8f599afe32fb77f2d6a2877bac0ac1e3cbf1b25
Stefan Kurtz <stefan.kurtz@uni-hamburg.de>
2025-05-02 14:09:59 +0200

    use const ref for filename and use local var gz_filename instead
    replace use of StrFormat by string concatenation

--- src/utilities/write_output_file.hpp
+++ src/utilities/write_output_file.hpp
@@ -4,45 +4,38 @@
 #include <zlib.h>
 #include <cstdio>
 #include <string>
-#include "utilities/str_format.hpp"
 
 /*
 ** A helper function to write (GZip) compressed or uncompressed output-files.
 ** compression_level=0 will result in uncompressed output.
 ** .gz file extensions will be automatically added where appropriate.
  */
-static void write_to_output_file(std::string file_name,
+static void write_to_output_file(const std::string &file_name,
                                  const std::string &content,
                                  size_t compression_level)
 {
   if(compression_level > 9)
   {
-    StrFormat msg(": GZip compression level can not be greater than 9!");
-    throw msg.str();
+    throw std::string(": GZip compression level can not be greater than 9!");
   }
   if(compression_level == 0)
   {
     FILE *f_out = fopen(file_name.c_str(), "w");
     if(f_out == NULL)
     {
-      StrFormat msg(": Error writing to file: %s", file_name.c_str());
-      throw msg.str();
+      throw std::string(": Error writing to file: ") + file_name;
     }
-
     fputs(content.c_str(), f_out);
     fclose(f_out);
-  }else
+  } else
   {
-    file_name += ".gz";
-
-    const std::string write_mode = "w"+std::to_string(compression_level);
-    gzFile f_out = gzopen(file_name.c_str(), write_mode.c_str());
+    const std::string gz_file_name = file_name + ".gz";
+    const std::string write_mode = "w" + std::to_string(compression_level);
+    gzFile f_out = gzopen(gz_file_name.c_str(), write_mode.c_str());
     if(f_out == Z_NULL)
     {
-      StrFormat msg(": Error writing to file: %s", file_name.c_str());
-      throw msg.str();
+      throw std::string(": Error writing to file: ") + gz_file_name;
     }
-
     gzputs(f_out, content.c_str());
     gzclose(f_out);
   }
