aaf8d4e33bad61ac7994f34e660a2b2841500d03
Stefan Kurtz <stefan.kurtz@uni-hamburg.de>
2025-05-27 01:09:31 +0200

    Allow to call guess_if_protein_file independent of main

--- scripts/guess_if_protein_seq.py
+++ scripts/guess_if_protein_seq.py
@@ -3,17 +3,6 @@
 import sys, argparse
 from fastaIterator import fasta_next
 
-def parse_arguments(argv):
-  p = argparse.ArgumentParser(description=('decide if input file contains '
-                                           'protein sequences'))
-  p.add_argument('-p','--print_result',action='store_true',default=False,
-                  help=('show result by printing 1 (for true) or 0 (for false) '
-                        ' and always exit with code 0, if no other problems '
-                        'occur'))
-  p.add_argument('inputfiles',nargs='+',
-                  help='specify input files, - means stdin')
-  return p.parse_args(argv)
-
 def guess_if_protein_sequence(guess_size_to_decide,sequence):
   protein_only_characters = set(list('LIFEQPXZ'))
   for _, cc in zip(range(guess_size_to_decide),sequence):
@@ -40,12 +29,24 @@ def guess_if_protein_file(inputfiles):
       break
   return decided_if_protein
 
-args = parse_arguments(sys.argv[1:])
+def guess_protein_parse_command_line(argv):
+  p = argparse.ArgumentParser(description=('decide if input file contains '
+                                           'protein sequences'))
+  p.add_argument('-p','--print_result',action='store_true',default=False,
+                  help=('show result by printing 1 (for true) or 0 (for false) '
+                        ' and always exit with code 0, if no other problems '
+                        'occur'))
+  p.add_argument('inputfiles',nargs='+',
+                  help='specify input files, - means stdin')
+  return p.parse_args(argv)
 
-is_protein = guess_if_protein_file(args.inputfiles)
-if args.print_result:
-  print(1 if is_protein else 0)
-else:
-  if is_protein:
-    exit(0)
-  exit(1)
+
+if __name__ == '__main__':
+  args = guess_protein_parse_command_line(sys.argv[1:])
+  is_protein = guess_if_protein_file(args.inputfiles)
+  if args.print_result:
+    print(1 if is_protein else 0)
+  else:
+    if is_protein:
+      exit(0)
+    exit(1)
