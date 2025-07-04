0cceec14eb2d16bcf47c659a1f0928f804f11bf4
Stevan Miladinovic <stevan.miladinovic@uni-hamburg.de>
2025-07-04 20:12:23 +0200

    Update $GTTL path to be relative, even if previously set as absolute path.

--- config.mk
+++ config.mk
@@ -1,4 +1,10 @@
+# If no path is defined in the environment, we attempt to use the git root, or otherwise
+# the folder named "gttl" in pwd.
 GTTL ?= $(shell git rev-parse --show-toplevel 2>/dev/null || pwd | awk -F'gttl' '/gttl/ {print $$1 "gttl"}')
+# Then, no matter what the variable previously was, we forcibly convert it
+# to a relative path. This fixes issues where file-paths are diff'd in testing scripts.
+GTTL := $(shell python3 -c "import os; print(os.path.relpath('$(GTTL)', os.getcwd()))")
+
 export PYTHONPATH=${GTTL}/scripts
 
 include ${GTTL}/compile_flags.mk
