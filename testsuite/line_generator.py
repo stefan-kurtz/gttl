63dadd0154f8f841e11cd34106f40f2e2d7af986
Stefan Kurtz <stefan.kurtz@uni-hamburg.de>
2025-07-25 11:23:39 +0200

    Removed code related to LineIterator
    rename test_fastq_iterator.sh -> test_fastq_generator.sh
    polishing of line_generator.py

--- testsuite/line_generator.py
+++ testsuite/line_generator.py
@@ -8,10 +8,12 @@ print('TMPFILE_src=`mktemp --tmpdir=.` || exit 1')
 print('TMPFILE_target=`mktemp --tmpdir=.` || exit 1')
 for filename in sys.argv[1:]:
   print('cp {} ${{TMPFILE_src}}'.format(filename))
-  print('./line_generator.x ${TMPFILE_src} | diff --strip-trailing-cr - ${TMPFILE_src}')
+  print(('./line_generator.x ${TMPFILE_src} | diff --strip-trailing-cr '
+         '- ${TMPFILE_src}'))
   for copy_num in range(2,4):
     file_list = ' '.join(['${TMPFILE_src}'] * copy_num)
     print('cat {} > ${{TMPFILE_target}}'.format(file_list))
-    print('./line_generator.x {} | diff --strip-trailing-cr - ${{TMPFILE_target}}'.format(file_list))
+    print((f'./line_generator.x {file_list} | diff --strip-trailing-cr - '
+           ' ${TMPFILE_target}'))
 
 print('rm -f ${TMPFILE_src} ${TMPFILE_target}')
