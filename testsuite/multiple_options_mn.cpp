e00cfd9c4e2f58de549f1bd227c0d07e53a9644b
Stefan Kurtz <stefan.kurtz@uni-hamburg.de>
2025-07-25 13:52:09 +0200

    Add code using is_key() in multiple_options_mn.cpp => Problem?

--- testsuite/multiple_options_mn.cpp
+++ testsuite/multiple_options_mn.cpp
@@ -2,32 +2,40 @@
 #include "utilities/find_lit_string.hpp"
 #include "utilities/multiple_options.hpp"
 
+static constexpr const GttlLitStringInitializerList display_option_args
+{
+  "k_mer",
+  "k_mer_counts",
+  "log2FC",
+  "p_value",
+  "p_value_histogram"
+};
+
+using DisplayOptions = MultipleOptions<display_option_args>;
+
 int main(int argc, char *argv[])
 {
-  static constexpr const GttlLitStringInitializerList display_option_args
-  {
-    "k_mer",
-    "k_mer_counts",
-    "log2FC",
-    "p_value",
-    "p_value_histogram"
-  };
-  MultipleOptions<display_option_args> display_options("");
-
+  DisplayOptions display_options("");
   if (argc != 2)
   {
     std::cerr << argv[0] << (": missing argument specifying + separated values "
                              "in single string, possible values are: ")
-                         << display_options.help_string() << std::endl;
+                         << display_options.help_string()
+                         << std::endl;
     return EXIT_FAILURE;
   }
   const std::string argstring(argv[1]);
   try
   {
     display_options.set_flags(argstring);
-    (void)display_options.to_string();
     std::cout << argv[0] << ": the following values are set: "
                          <<  display_options.to_string() << std::endl;
+#ifdef NEWCODE
+    if (display_options.is_set<"p_value">())
+    {
+      std::cout << "p_value is set" << '\n';
+    }
+#endif
   }
   catch (const std::invalid_argument &err)
   {
