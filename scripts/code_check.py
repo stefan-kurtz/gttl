70aa4a3491f181fdfec1b4eb36e1f4ec4cc774ea
Stefan Kurtz <stefan.kurtz@uni-hamburg.de>
2025-11-09 19:20:12 +0100

    New version of code_check.py

--- scripts/code_check.py
+++ scripts/code_check.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
 '''
-Eingabe: Dateinamen mit Programmcode (z.B. in Python oder C). Finde
-Formatierungsfehler in der Datei und breche beim ersten solchen Fehler
-ab. Die folgendne Fehler werden mit einer Zeilennummer und einer entsprechenden
-Meldung berichtet und das Programm bricht mit Fehlercode 1 ab:
+Eingabe: Dateinamen mit Programmcode (z.B. in Python oder C).
+Finde Formatierungsfehler in der Datei und breche mit exit(1) ab,
+wenn es mindestens einen Fehler gibt.
+Es werden bis zum 10 Fehler gesammelt und ausgegeben, bevor abgebraochen wird.
+Die folgendne Fehler werden mit einer Zeilennummer und einer entsprechenden
+Meldung berichtet:
+
 - Zeilen, die mehr als 80 Zeichen lang sind
 
 optional:
@@ -21,62 +24,67 @@ Ein Argument - wird als sys.stdin interpretiert.
 import sys, re, argparse
 
 def parse_command_line(argv):
-  p = argparse.ArgumentParser()
-  p.add_argument('-w','--trailing_white_space',action='store_true',
-                 default=False,help='check for trailing white spaces')
-  p.add_argument('-i','--indent_level',action='store_true',
-                 default=False,help='check for indentation level')
-  p.add_argument('-t','--no_tabulator',action='store_true',
-                 default=False,help='check for occurrence of tabulator')
-  p.add_argument('inputfile',nargs='+',
+  p = argparse.ArgumentParser(description=('report error if Python/C++-code '
+                                           'contains lines longer than 80 '
+                                           'characters; optionally errors are '
+                                           'reported due to trailing white '
+                                           'spaces, inconsistent indentation '
+                                           'levels or occurrence of '
+                                           'tabulators'))
+  p.add_argument('-w', '--trailing_white_space', action='store_true',
+                 default=False, help='check for trailing white spaces')
+  p.add_argument('-i', '--indent_level', action='store_true',
+                 default=False, help='check for indentation level')
+  p.add_argument('-t', '--no_tabulator', action='store_true',
+                 default=False, help='check for occurrence of tabulator')
+  p.add_argument('inputfile', nargs='+',
                   help='specify input file, - means stdin')
   return p.parse_args(argv)
 
-def errormsg(error_list,filename,linenum,msg):
-  error_list.append('file {}, line {}: {}'.format(filename,linenum,msg))
+def errormsg(error_list, filename, linenum, msg):
+  error_list.append('file {}, line {}: {}'.format(filename, linenum, msg))
 
-def code_check(error_list,trailing_white_space,filename):
-  if filename == '-':
-    stream = sys.stdin
-  else:
-    #sys.stderr.write('{}: open {}\n'.format(sys.argv[0],filename))
+def code_check(error_list, trailing_white_space, filename):
+  if filename != '-':
     try:
       stream = open(filename)
     except IOError as err:
-      sys.stderr.write('{}: {}\n'.format(sys.argv[0],err))
+      sys.stderr.write(f'{sys.argv[0]}: {err}\n')
       exit(1)
+  else:
+    stream = sys.stdin
   linenum = 0
   for line in stream:
     if len(error_list) >= 10:
       break
     line = line.rstrip('\n')
     linenum += 1
-    if args.no_tabulator and re.search(r'\t',line):
-      errormsg(error_list,filename,linenum,'line contains tabulator symbol')
-    if trailing_white_space and re.search(r'\s+$',line):
-      errormsg(error_list,filename,linenum,'trailing white space')
-    if len(line) > 80 and (not line.startswith('#include')):
-      errormsg(error_list,filename,linenum,'line longer than 80 characters')
+    if args.no_tabulator and re.search(r'\t', line):
+      errormsg(error_list, filename, linenum, 'line contains tabulator symbol')
+    if trailing_white_space and re.search(r'\s+$', line):
+      errormsg(error_list, filename, linenum, 'trailing white space')
+    if len(line) > 80 and (not line.startswith('#')):
+      errormsg(error_list, filename, linenum, 'line longer than 80 characters')
     if args.indent_level:
-      m = re.search(r'(^\s+)(\S)',line)
-      if m:
-        indent = m.group(1)
+      if mo := re.search(r'(^\s+)(\S)', line):
+        indent = mo.group(1)
         indent_size = len(indent)
         if indent_size % 2 != 0 and indent_size % 4 != 0:
-          first_char = m.group(2)
+          first_char = mo.group(2)
           if not (first_char in '."#\''):
-            errormsg(error_list,filename,linenum,\
+            errormsg(error_list, filename, linenum,
                      'indentation level must be multiples of 2 or 4')
-  stream.close()
+  if filename != '-':
+    stream.close()
 
 args = parse_command_line(sys.argv[1:])
 
 error_list = list()
 for filename in args.inputfile:
-  code_check(error_list,args.trailing_white_space,filename)
+  code_check(error_list, args.trailing_white_space, filename)
 
 for err_msg in error_list:
-  sys.stderr.write('{}: {}\n'.format(sys.argv[0],err_msg))
+  sys.stderr.write(f'{sys.argv[0]}: {err_msg}\n')
 
 if len(error_list) > 0:
   exit(1)
