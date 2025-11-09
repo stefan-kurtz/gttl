0d6ff4d2397a202bfc48115e7a003b3e0bf312c0
Stefan Kurtz <stefan.kurtz@uni-hamburg.de>
2025-11-09 19:43:32 +0100

    Simplify output of compilation step

--- compile_targets.mk
+++ compile_targets.mk
@@ -1,7 +1,8 @@
 %.o: %.cpp
-	$(CXX) $(TIME_OPTION) -DCXX_FLAGS=\"'$(CXXFLAGS) $(CFLAGS) $(CPPFLAGS)'\" -c $< -o $@ $(CXXFLAGS) $(CFLAGS) $(CPPFLAGS) -MT $@ -MMD -MP -MF $(@:.o=.d)
+	@echo "${CXX} $< => $@"
+	@$(CXX) $(TIME_OPTION) -DCXX_FLAGS=\"'$(CXXFLAGS) $(CFLAGS) $(CPPFLAGS)'\" -c $< -o $@ $(CXXFLAGS) $(CFLAGS) $(CPPFLAGS) -MT $@ -MMD -MP -MF $(@:.o=.d)
 
 # In the simplest case, an executable consists of 1 object.
 %.x: %.o
-	$(LD) $(LDFLAGS) $< -o $@ $(LDLIBS)
-
+	@echo "${LD} $< => $@"
+	@$(LD) $(LDFLAGS) $< -o $@ $(LDLIBS)
