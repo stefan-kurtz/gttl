7ab100cdbaa237f601e145f69a11ef52f605c8e6
Stefan Kurtz <stefan.kurtz@uni-hamburg.de>
2025-12-04 15:09:05 +0100

    Added class to maintain a thread specific index from 0 to num_threads-1
    this can be looked up when running the function in a thread who
    takes the tasks from a thread pool with an unknown number of tasks

---
+++ src/threading/thread_specific_index.hpp
@@ -0,0 +1,33 @@
+#ifndef THREAD_SPECIFIC_INDEX_HPP
+#define THREAD_SPECIFIC_INDEX_HPP
+#include <cstddef>
+#include <cassert>
+#include <mutex>
+#include <map>
+
+class ThreadSpecificIndex
+{
+  size_t num_threads;
+  std::mutex thread_id_mutex{};
+  std::map<std::thread::id, size_t> thread_id_map;
+  public:
+  ThreadSpecificIndex(size_t _num_threads)
+    : num_threads(_num_threads)
+  { }
+  size_t get(void)
+  {
+    const std::thread::id t_id = std::this_thread::get_id();
+    thread_id_mutex.lock();
+    if (thread_id_map.size() < num_threads)
+    {
+      const size_t current_size = thread_id_map.size();
+      thread_id_map[t_id] = current_size;
+    } else
+    {
+      assert(thread_id_map.size() == num_threads);
+    }
+    thread_id_mutex.unlock();
+    return thread_id_map[t_id];
+  }
+};
+#endif
