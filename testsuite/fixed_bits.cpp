5158d2e5b6516f4a19e95b282b74126ef93d5825
Stefan Kurtz <stefan.kurtz@uni-hamburg.de>
2025-12-22 12:08:35 +0100

    Added FixedBitClass with union

---
+++ testsuite/fixed_bits.cpp
@@ -0,0 +1,57 @@
+#include <cstdint>
+#include <string>
+#include <climits>
+#include <iostream>
+#include <cstring>
+#include <format>
+#include <cassert>
+
+class FixedBitClass
+{
+  public:
+  static constexpr const int num_bits = 36;
+  private:
+  static constexpr const int num_bytes = (2 * num_bits + CHAR_BIT - 1)/CHAR_BIT;
+  struct TwoValues
+  {
+    uint64_t a:num_bits,
+             b:num_bits;
+  };
+  union
+  {
+    uint8_t vector[num_bytes];
+    TwoValues two_values;
+  } overlay;
+  public:
+  FixedBitClass(uint64_t _a, uint64_t _b)
+  {
+#ifndef NDEBUG
+    constexpr const uint64_t max_value = (uint64_t(1) << num_bits) - 1;
+#endif
+    assert(_a <= max_value and _b <= max_value);
+    overlay.two_values.a = _a;
+    overlay.two_values.b = _b;
+  }
+  std::string to_string(void) const
+  {
+    std::string s = "(" + std::to_string(overlay.two_values.a) +
+                    "," + std::to_string(overlay.two_values.b) +
+                    ")";
+    for (int idx = 0; idx < num_bytes; idx++)
+    {
+      s += std::format(" {:b}",overlay.vector[idx]);
+    }
+    return s;
+  }
+  bool operator <(const FixedBitClass &other) const
+  {
+    return memcmp(&overlay.vector[0],&other.overlay.vector[0],num_bytes);
+  }
+};
+
+int main(void)
+{
+  const uint64_t max_value = (uint64_t(1) << FixedBitClass::num_bits);
+  FixedBitClass fixed_bits(max_value, max_value);
+  std::cout << fixed_bits.to_string() << '\n';
+};
