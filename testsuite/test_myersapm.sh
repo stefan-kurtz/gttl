6d2fd18c5bb49db5be4f358a43f5e1592c37d695
Stefan Kurtz <stefan.kurtz@uni-hamburg.de>
2026-07-11 20:16:05 +0200

    replace echo involving \n by printf-command in shell script

--- testsuite/test_myersapm.sh
+++ testsuite/test_myersapm.sh
@@ -7,8 +7,8 @@ TMPFILE2=`mktemp --tmpdir=. TMP.XXXXXX` || exit 1
 TMPFILE3=`mktemp --tmpdir=. TMP.XXXXXX` || exit 1
 TMPFILE4=`mktemp --tmpdir=. TMP.XXXXXX` || exit 1
 echo "atggc" > ${TMPFILE1}
-echo ">\naggtatcgc" > ${TMPFILE2}
-echo "atggc	5" > ${TMPFILE3}
+printf ">\naggtatcgc\n" > ${TMPFILE2}
+printf "atggc\t5\n" > ${TMPFILE3}
 for strategy in p_times_s s_times_p
 do
   ./myersapm.x ${strategy} 1 ${TMPFILE1} ${TMPFILE2} 2 > ${TMPFILE4}
