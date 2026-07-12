dcb53ab39e85a81ff13a714fafca4e9bfbc14bb8
Stefan Kurtz <stefan.kurtz@uni-hamburg.de>
2026-07-12 14:11:46 +0200

    removed set -e -x from runskylines.sh and check error code

--- tools/suffixarrays/runskylines.sh
+++ tools/suffixarrays/runskylines.sh
@@ -1,26 +1,40 @@
 #!/bin/sh
 
-set -e -x
+if test ! -d ${PACKAGES}/libdivsufsort/lib
+then
+  exit 0
+else
+  export DYLD_LIBRARY_PATH=${PACKAGES}/libdivsufsort/lib
+fi
 
 # compare sais lite and libdivsufsort for skyline strings.
 
 param=3
-export DYLD_LIBRARY_PATH=${PACKAGES}/libdivsufsort/lib
 while test $param -ne 25
 do
   for direction in "" "--reverse"
   do
     echo "skyline ${direction} ${param}"
-    outputfile=skyline${direction}-${param}.txt
-    ./skyline.py ${direction} ${param} > ${outputfile}
+    TMPFILE=`mktemp --tmpdir=. TMP.XXXXXX` || exit 1
+    ./skyline.py ${direction} ${param} > ${TMPFILE}
+    if test $? -ne 0
+    then
+      echo "FAILURE: ./skyline.py ${direction} ${param}"
+      exit 1
+    fi
     for lcptab in "" "--lcptab kasai13n --check_suftab" "--absolute_suftab --lcptab kasai9n" "--absolute_suftab --lcptab plcp5n"
     do
-      ./sa_induced.x -v --plain_input_format ${lcptab} ${suftab} ${outputfile}
+      ./sa_induced.x -v --plain_input_format ${lcptab} ${suftab} ${TMPFILE}
+      if test $? -ne 0
+      then
+        echo "FAILURE: ./sa_induced.x -v --plain_input_format ${lcptab} ${suftab} ${TMPFILE}"
+        exit 1
+      fi
     done
-    rm -f ${outputfile}
+    rm -f ${TMPFILE}
     for suffix in suf ll2 ll4 lcp prj
     do
-      rm -f ${outputfile}.${suffix}
+      rm -f ${TMPFILE}.${suffix}
     done
   done
   param=`expr $param + 1`
